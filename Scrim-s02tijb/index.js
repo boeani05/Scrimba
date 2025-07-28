97c58fc97717d7899d667c1243133fc71b366d3b
boeani05 <flyingdoge@gmx.at>
2025-07-28 10:28:45 +0200

    adjusted the radio buttons accordingly and made the gif button interact with the user

--- Scrim-s02tijb/index.js
+++ Scrim-s02tijb/index.js
@@ -1,11 +1,33 @@
 import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImgBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
+getImgBtn.addEventListener('click', getMatchingCatsArray)
+
 function highlightCheckedOption(e) {
+    const radios = document.getElementsByClassName('radio')
+    
+    for (let radio of radios) {
+        radio.classList.remove('highlight')
+    }
+
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function getMatchingCatsArray() {
+
+    const isGif = gifsOnlyOption.checked
+
+    console.log(isGif)
+
+    if(document.querySelector('input[type="radio"]:checked')) {
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        console.log(selectedEmotion)
+    }
 }
 
 function getEmotionsArray(cats) {
